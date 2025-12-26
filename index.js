95850f45322fda7423dfee6632c38529829b0036
ZaibHusan <222094199+ZaibHusan@users.noreply.github.com>
2025-12-26 04:16:30 -0800

    Final metion has been added

--- index.js
+++ index.js
@@ -15,13 +15,10 @@ const PORT = 3000;
 
 
 app.use(cors({
-    origin: [
-        "http://localhost:5173",
-        "http://localhost:5174",
-        "https://zebify-admin.vercel.app/",
-        "http://127.0.0.1:5173"
-    ],
-    credentials: true
+  origin: function (origin, callback) {
+    callback(null, origin); // allow ANY origin dynamically
+  },
+  credentials: true
 }));
 
 connectMongoose();
